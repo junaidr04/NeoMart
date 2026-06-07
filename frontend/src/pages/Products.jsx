bbc59ac591a0c978b4effee1469e3dccaf095367
junaidr04 <jbj.jawad@gmail.com>
2026-06-07 14:52:57 +0600

    Fix duplicate import in Products

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import { useCart } from "../context/CartContext";
-import { useNavigate } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
 import api from "../api";
 
